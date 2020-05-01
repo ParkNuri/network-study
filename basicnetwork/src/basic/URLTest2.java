c85342f742256a72fdac296a9aee51c6160b28c2
ParkNuri <qkrsnrl@naver.com>
2020-05-01 18:13:10 +0900

    network study

---
+++ basicnetwork/src/basic/URLTest2.java
@@ -0,0 +1,50 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+// URL클래스
+// => 인터넷에서 확인할 수 있는 자원을 추출
+public class URLTest2 {
+
+	public static void main(String[] args) {
+		try {
+			URL url = new URL("https://www.daum.net");
+			System.out.println(url.toString());
+			System.out.println("url.getHost()=>"+url.getHost());
+			System.out.println("url.getPath()=>"+url.getPath());
+			// port가 -1
+			// -1포트로 접속했다는 의미가 아니라 http의 기본 포트가 80port
+			// 우리가 접속한 url이 기본 포트를 사용하고 있다는 의미
+			System.out.println("url.getPort()=>"+url.getPort());
+			System.out.println("url.getProtocol()=>"+url.getProtocol());
+			System.out.println("url.getFile()=>"+url.getFile());
+			
+			// 지정한 url에서 자원을 읽어오기 - 한글이 깨진다
+			// Reader의 하위 클래스를 추가해서 읽어오기 - 이미지, 동영상 ...
+			
+			InputStream data = url.openStream();
+			// byte -> 한 문자
+			InputStreamReader isr = new InputStreamReader(data);
+			BufferedReader br = new BufferedReader(isr);
+			while(true) {
+				String databyte = br.readLine();
+				if(databyte==null) {
+					break;
+				}
+				System.out.println(databyte);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}	// MalformedURLException : url 형식이 잘못되었을때
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
